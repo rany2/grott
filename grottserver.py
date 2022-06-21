d833df2ac68a7ff52e1aeed53aa35a8015602012
rany <ranygh@riseup.net>
2022-06-21 18:36:28 +0300

    fix error when automatic closing of connection was impl

--- grottserver.py
+++ grottserver.py
@@ -872,7 +872,7 @@ class GrowattServerHandler(socketserver.BaseRequestHandler):
         # Verify CRC16
         if not crc16_verify(data):
             print("\t - Grottserver - CRC16 failed")
-            self.shutdown_queue.put_nowait(True)
+            self.shutdown_queue[self.qname].put_nowait(True)
             return
 
         # Collect data for MQTT, PVOutput, InfluxDB, etc..
