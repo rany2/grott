90ab397c537fccd036cc4ed2baa59a2c899a9730
egguy <crazypops@gmail.com>
2022-11-22 00:24:18 +0100

    Updated grott_ha version

--- examples/Home Assistent/grott_ha.py
+++ examples/Home Assistent/grott_ha.py
@@ -9,7 +9,7 @@ from paho.mqtt.publish import single, multiple
 from grottconf import Conf
 from grottdata import pr
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 """A pluging for grott
 This plugin allow to have autodiscovery of the device in HA
